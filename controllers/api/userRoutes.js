e996073df93f55d5e8d2ad16ea26d51e408bf703
Jack Smith <jack_smith21554@yahoo.com>
2023-07-12 15:43:36 -0400

    Added bootstrap to main.handlebars and begun in-line styling for each page. Continued troubleshooting session log in issues.

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,15 +1,12 @@
 const router = require('express').Router();
 const { Users } = require('../../models');
 
-router.post('/', async (req, res) => {
+router.post('/signup', async (req, res) => {
     try {
-        const userData = await User.create(req.body);
-
-        req.session.save(() => {
-            req.session.user_id = userData.id;
-            req.session.logged_in = true;
-            res.status(200).json(userData);
+        const newUser = await User.create({
+            ...req.body,
         });
+        res.status(200).json(newUser);
     } catch(err) {
         res.status(400).json(err);
     }
--- public/js/login.js
+++ public/js/login.js
@@ -1,5 +1,4 @@
 const loginForm = document.querySelector('.login-form');
-const singupForm = document.querySelector('.signup-form');
 
 const loginFormHandler = async (event) => {
     event.preventDefault();
@@ -15,34 +14,11 @@ const loginFormHandler = async (event) => {
         });
 
         if (response.ok) {
-            document.location.replace('/profile');
+            document.location.replace('/');
         } else {
             alert(response.statusText);
         }
     }
 };
 
-const signupFormHandler = async (event) => {
-    event.preventDefault();
-
-    const name = document.querySelector('#name-signup').value.trim();
-    const username = document.querySelector('#user-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-
-    if (name && username && password) {
-        const response = await fetch('/api/user', {
-            method: 'POST',
-            body: JSON.stringify({ name, username, password }),
-            headers: { 'Content-Type': 'application/json' },
-        });
-
-        if (response.ok) {
-            document.location.replace('/profile');
-        } else {
-            alert(response.statusText);
-        }
-    }
-}
-
 loginForm.addEventListener('submit', loginFormHandler);
-singupForm.addEventListener('submit', signupFormHandler);
